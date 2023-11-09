fdfbcb52d5750f6a9564e86b7f04d2b731e4cfdc
Jared Drueco <drueco03@gmail.com>
2023-11-08 22:42:23 -0700

    Deadlock free solution using cond var

--- cond_var/main.cpp
+++ cond_var/main.cpp
@@ -8,48 +8,54 @@ using namespace std;
 
 bool producerDone = false;
 queue<int> buffer;
-int bufferSize = 5;
+int bufferSize = 10;
+
 pthread_mutex_t mutexBuffer = PTHREAD_MUTEX_INITIALIZER;
-//pthread_cond_t condNotEmpty = PTHREAD_COND_INITIALIZER;
-//pthread_cond_t condNotFull = PTHREAD_COND_INITIALIZER;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t condNotEmpty = PTHREAD_COND_INITIALIZER;
+pthread_cond_t condNotFull = PTHREAD_COND_INITIALIZER;
 
 void producer() {
-	ifstream inputFile("input");
-	char command;
-	int n;
-	while (inputFile >> command >> n) {
-		pthread_mutex_lock(&mutexBuffer);
-		while (buffer.size() == bufferSize) {
-			cout << "Waiting for space in buffer..." << endl;
-			//pthread_cond_wait(&condNotFull, &mutexBuffer);
-			pthread_cond_wait(&cond, &mutexBuffer);
-		}
-		buffer.push(n);
-		pthread_mutex_unlock(&mutexBuffer);
-		//pthread_cond_signal(&condNotEmpty);
-		pthread_cond_signal(&cond);
-	}
-	producerDone = true;
+    ifstream inputFile("input");
+    char command;
+    int n;
+    while (inputFile >> command >> n) {
+        pthread_mutex_lock(&mutexBuffer);
+        while (buffer.size() == bufferSize) {
+            cout << "Waiting for space in buffer..." << endl;
+            pthread_cond_wait(&condNotFull, &mutexBuffer);
+        }
+        buffer.push(n);
+        pthread_cond_signal(&condNotEmpty);
+        pthread_mutex_unlock(&mutexBuffer);
+    }
+
+    pthread_mutex_lock(&mutexBuffer);
+    producerDone = true;
+    pthread_cond_broadcast(&condNotEmpty); // Wake up all consumers
+    pthread_mutex_unlock(&mutexBuffer);
 }
 
 void* consumer(void* args) {
-	while (!producerDone) {
-		pthread_mutex_lock(&mutexBuffer);
-		while (buffer.empty()) {
-			cout << "Waiting for tasks in buffer..." << endl;
-			//pthread_cond_wait(&condNotEmpty, &mutexBuffer);
-			pthread_cond_wait(&cond, &mutexBuffer);
-		}
-		int task = buffer.front();
-		cout << task << endl;
-		buffer.pop();
-		pthread_mutex_unlock(&mutexBuffer);
-		//pthread_cond_signal(&condNotFull);
-		pthread_cond_signal(&cond);
-	}
-	return NULL;
+    while (1) {
+        pthread_mutex_lock(&mutexBuffer);
+        while (buffer.empty() && !producerDone) {
+            cout << "Waiting for tasks in buffer..." << endl;
+            pthread_cond_wait(&condNotEmpty, &mutexBuffer);
+        }
+        if (producerDone && buffer.empty()) {
+            pthread_mutex_unlock(&mutexBuffer);
+            break;
+        }
+        int task = buffer.front();
+        buffer.pop();
+        pthread_cond_signal(&condNotFull);
+        pthread_mutex_unlock(&mutexBuffer);
+
+        cout << "Processing task " << task << endl;
+    }
+    return NULL;
 }
+
 
 int main() {
 	int nthreads = 3;
@@ -69,6 +75,8 @@ int main() {
 		}
 	}
 	pthread_mutex_destroy(&mutexBuffer);
+	pthread_cond_destroy(&condNotEmpty);
+	pthread_cond_destroy(&condNotFull);
 
 	return 0;
 }

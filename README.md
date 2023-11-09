480740ca001bcd713a0c81fdf5b8ce7a70d17984
Jared Drueco <drueco03@gmail.com>
2023-11-09 15:37:13 -0700

    Added docs

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Bound 2
+
+This project provides **two** solutions to a classic multithreading synchronization problem in operating systems called the [Bounded Buffer Problem](https://en.wikipedia.org/wiki/Producer%E2%80%93consumer_problem).
+
+This project models a scenario with one producer and multiple consumers who share a buffer. The producer places work in the buffer, while consumers retrieve and process work. The challenge is to prevent the producer from adding work to a full buffer and consumers from trying to remove work from an empty buffer, all while ensuring proper synchronization.
+
+### Solution 1 - Mutex & Condition Variables
+
+The `cond_var` directory is a solution that uses condition variables and mutexes.
+```bash
+cd cond_var
+make
+./bound-2-cond-var <nthreads>
+```
+nthreads is the number of consumer threads to use.
+
+
+### Solution 2 - Mutex & Semaphores
+
+WIP
+
+### References
+[1] UAlberta CMPUT 379 - Operating Systems, Assignment 2
--- cond_var/main.cpp
+++ cond_var/main.cpp
@@ -1,3 +1,9 @@
+/**
+ * This program simulates a producer and multiple consumers that share a buffer.
+ * The producer generates work or sleeps, adds them to the buffer, and consumers retrieve
+ * work items from the buffer, processing them. It uses mutexes and condition variables
+ * for synchronization.
+ */
 #include <iostream>
 #include <fstream>
 #include <pthread.h>
@@ -8,12 +14,26 @@ using namespace std;
 
 bool producerDone = false;
 queue<int> buffer;
-int bufferSize = 10;
+int bufferSize;
 
 pthread_mutex_t mutexBuffer = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t condNotEmpty = PTHREAD_COND_INITIALIZER;
 pthread_cond_t condNotFull = PTHREAD_COND_INITIALIZER;
 
+/**
+ * This function reads commands and values from an input file and produces
+ * work items or sleeps, then adds them to a shared buffer. It uses mutexes
+ * and condition variables for synchronization.
+ *
+ * Algorithm
+ *
+ * 1. Acquire the mutex lock.
+ * 2. If the buffer is full, the producer waits on a condition variable, which
+ *    internally releases the mutex and waits for the condition signal. Once
+ *    signalled and the buffer is not full, the mutex is re-acquired.
+ * 3. The producer then adds an item to the buffer and signals the condition
+ *    variable that the buffer is not empty, and releases the mutex.
+ */
 void producer() {
     ifstream inputFile("input");
     char command;
@@ -21,11 +41,11 @@ void producer() {
     while (inputFile >> command >> n) {
 		pthread_mutex_lock(&mutexBuffer);
 		while (buffer.size() == bufferSize) {
-			cout << "Waiting for space in buffer..." << endl;
+			printf("Producer waiting for space in buffer...\n");
 			pthread_cond_wait(&condNotFull, &mutexBuffer);
 		}
         if (command == 'T') {
-            printf("Producer puts work %d\n", n);
+            printf("Producer puts work %d\n in buffer", n);
 		    buffer.push(n);
         } else if (command == 'S') {
             printf("Producer sleeps %d\n", n);
@@ -34,19 +54,38 @@ void producer() {
 		pthread_cond_signal(&condNotEmpty);
 		pthread_mutex_unlock(&mutexBuffer);
     }
-    pthread_mutex_lock(&mutexBuffer);
     producerDone = true;
     pthread_cond_broadcast(&condNotEmpty);
-    pthread_mutex_unlock(&mutexBuffer);
     printf("Producer end\n");
 }
 
+/**
+ * This routine represents a consumer thread that retrieves and processes work
+ * items from a shared buffer. It uses mutexes and condition variables for
+ * synchronization.
+ *
+ * Algorithm
+ *
+ * 1. Acquire the mutex lock.
+ * 2. If the buffer is empty and the producer is not done, the producer waits on
+ *    a condition variable, which internally releases the mutex and waits for
+ *    the condition signal. Once signalled and the is at least one item in the
+ *    buffer and the producer is not done, the mutex is re-acquired.
+ * 3. The consumer now receives the work from the buffer and signals the
+ *    condition variable that buffer is not full. It now performs work on
+ *    received task.
+ *
+ * @param args A pointer to an integer representing the unique identifier of
+ * this consumer.
+ * @return NULL when the consumer thread has finished processing work.
+ */
 void* consumer(void* args) {
     int id = *((int*)args);
     while (1) {
         pthread_mutex_lock(&mutexBuffer);
         printf("Consumer %d asks for work\n", id);
         while (buffer.empty() && !producerDone) {
+			printf("Consumer %d waiting for work in buffer...\n", id);
             pthread_cond_wait(&condNotEmpty, &mutexBuffer);
         }
         if (producerDone && buffer.empty()) {
@@ -64,8 +103,10 @@ void* consumer(void* args) {
     return NULL;
 }
 
-int main() {
-	int nthreads = 3;
+int main(int argc, char* argv[]) {
+    int nthreads = atoi(argv[1]);
+    bufferSize = 2 * nthreads;
+
 	pthread_t consumerTh[nthreads];
     int consumerId[nthreads];
 

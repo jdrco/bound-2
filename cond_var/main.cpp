2f75e67ad15322a26d7746f56dee4e389b0ea6f7
Jared Drueco <drueco03@gmail.com>
2023-11-09 15:42:53 -0700

    Added time before every print

--- cond_var/main.cpp
+++ cond_var/main.cpp
@@ -9,6 +9,8 @@
 #include <pthread.h>
 #include <queue>
 #include "tands.h"
+#include <chrono>
+#include <ctime>
 
 using namespace std;
 
@@ -39,24 +41,24 @@ void producer() {
     char command;
     int n;
     while (inputFile >> command >> n) {
-		pthread_mutex_lock(&mutexBuffer);
-		while (buffer.size() == bufferSize) {
-			printf("Producer waiting for space in buffer...\n");
-			pthread_cond_wait(&condNotFull, &mutexBuffer);
-		}
+        pthread_mutex_lock(&mutexBuffer);
+        while (buffer.size() == bufferSize) {
+            printf("%.2f Producer waiting for space in buffer...\n", (double)clock() / CLOCKS_PER_SEC);
+            pthread_cond_wait(&condNotFull, &mutexBuffer);
+        }
         if (command == 'T') {
-            printf("Producer puts work %d\n in buffer", n);
-		    buffer.push(n);
+            printf("%.2f Producer puts work %d in buffer\n", (double)clock() / CLOCKS_PER_SEC, n);
+            buffer.push(n);
         } else if (command == 'S') {
-            printf("Producer sleeps %d\n", n);
+            printf("%.2f Producer sleeps %d\n", (double)clock() / CLOCKS_PER_SEC, n);
             Sleep(n);
         }
-		pthread_cond_signal(&condNotEmpty);
-		pthread_mutex_unlock(&mutexBuffer);
+        pthread_cond_signal(&condNotEmpty);
+        pthread_mutex_unlock(&mutexBuffer);
     }
     producerDone = true;
     pthread_cond_broadcast(&condNotEmpty);
-    printf("Producer end\n");
+    printf("%.2f Producer end\n", (double)clock() / CLOCKS_PER_SEC);
 }
 
 /**
@@ -69,11 +71,11 @@ void producer() {
  * 1. Acquire the mutex lock.
  * 2. If the buffer is empty and the producer is not done, the producer waits on
  *    a condition variable, which internally releases the mutex and waits for
- *    the condition signal. Once signalled and the is at least one item in the
+ *    the condition signal. Once signalled and there is at least one item in the
  *    buffer and the producer is not done, the mutex is re-acquired.
  * 3. The consumer now receives the work from the buffer and signals the
  *    condition variable that buffer is not full. It now performs work on
- *    received task.
+ *    the received task.
  *
  * @param args A pointer to an integer representing the unique identifier of
  * this consumer.
@@ -83,9 +85,9 @@ void* consumer(void* args) {
     int id = *((int*)args);
     while (1) {
         pthread_mutex_lock(&mutexBuffer);
-        printf("Consumer %d asks for work\n", id);
+        printf("%.2f Consumer %d asks for work\n", (double)clock() / CLOCKS_PER_SEC, id);
         while (buffer.empty() && !producerDone) {
-			printf("Consumer %d waiting for work in buffer...\n", id);
+            printf("%.2f Consumer %d waiting for work in buffer...\n", (double)clock() / CLOCKS_PER_SEC, id);
             pthread_cond_wait(&condNotEmpty, &mutexBuffer);
         }
         if (producerDone && buffer.empty()) {
@@ -96,9 +98,9 @@ void* consumer(void* args) {
         buffer.pop();
         pthread_cond_signal(&condNotFull);
         pthread_mutex_unlock(&mutexBuffer);
-        printf("Consumer %d receives task %d\n", id, task);
+        printf("%.2f Consumer %d receives task %d\n", (double)clock() / CLOCKS_PER_SEC, id, task);
         Trans(task);
-        printf("Consumer %d completes task %d\n", id, task);
+        printf("%.2f Consumer %d completes task %d\n", (double)clock() / CLOCKS_PER_SEC, id, task);
     }
     return NULL;
 }
@@ -107,26 +109,26 @@ int main(int argc, char* argv[]) {
     int nthreads = atoi(argv[1]);
     bufferSize = 2 * nthreads;
 
-	pthread_t consumerTh[nthreads];
+    pthread_t consumerTh[nthreads];
     int consumerId[nthreads];
 
-	for (int i = 0; i < nthreads; i++) {
+    for (int i = 0; i < nthreads; i++) {
         consumerId[i] = i;
-		if (pthread_create(&consumerTh[i], nullptr, &consumer, &consumerId[i]) != 0) {
-			cerr << "Failed to create thread" << endl;
-		}
-	}
+        if (pthread_create(&consumerTh[i], nullptr, &consumer, &consumerId[i]) != 0) {
+            cerr << "Failed to create thread" << endl;
+        }
+    }
 
-	producer();
+    producer();
 
-	for (int i = 0; i < nthreads; i++) {
-		if (pthread_join(consumerTh[i], nullptr) != 0) {
-			cerr << "Failed to join thread" << endl;
-		}
-	}
-	pthread_mutex_destroy(&mutexBuffer);
-	pthread_cond_destroy(&condNotEmpty);
-	pthread_cond_destroy(&condNotFull);
+    for (int i = 0; i < nthreads; i++) {
+        if (pthread_join(consumerTh[i], nullptr) != 0) {
+            cerr << "Failed to join thread" << endl;
+        }
+    }
+    pthread_mutex_destroy(&mutexBuffer);
+    pthread_cond_destroy(&condNotEmpty);
+    pthread_cond_destroy(&condNotFull);
 
-	return 0;
+    return 0;
 }
